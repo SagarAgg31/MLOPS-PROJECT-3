e766539931ebb2f53200440a6ab95536c4a45cad
SAGAR AGGARWAL <aggarwal3110@gmail.com>
2025-04-13 17:05:48 +0530

    Added Data Drift detection!

--- application.py
+++ application.py
@@ -3,6 +3,13 @@ import numpy as np
 import pandas as pd
 from flask import Flask,render_template,request,jsonify
 
+from alibi_detect.cd import KSDrift
+from src.feature_store import RedisFeatureStore
+from sklearn.preprocessing import StandardScaler
+from src.logger import get_logger
+
+logger = get_logger(__name__)
+
 app = Flask(__name__,template_folder='templates')
 
 MODEL_PATH = 'artifacts/models/random_forest.pkl'
@@ -11,6 +18,22 @@ with open(MODEL_PATH,'rb') as model_file:
 
 FEATURE_NAMES = ['Age', 'Fare', 'Pclass', 'Sex', 'Embarked', 'Familysize', 'Isalone',
        'HasCabin', 'Title', 'Pclass_Fare', 'Age_Fare']
+
+feature_store = RedisFeatureStore()
+scaler = StandardScaler()
+
+def fit_scaler_on_ref_data():
+    entity_ids = feature_store.get_all_entity_ids()
+    all_features = feature_store.get_batch_features(entity_ids)
+
+    all_features_df = pd.DataFrame.from_dict(all_features,orient='index')[FEATURE_NAMES]
+
+    scaler.fit(all_features_df)
+    return scaler.transform(all_features_df)
+
+historical_data = fit_scaler_on_ref_data()
+ksd = KSDrift(x_ref=historical_data,p_val=0.05)
+
 
 @app.route('/')
 def home():
@@ -36,6 +59,18 @@ def predict():
        HasCabin, Title, Pclass_Fare, Age_Fare]],columns=FEATURE_NAMES)
         
         prediction = model.predict(features)[0]
+
+        #### DATA DRIFT DETECTION
+        features_scaled = scaler.transform(features)
+
+        drift = ksd.predict(features_scaled)      
+        
+        drift_response = drift.get('data',{})
+        is_drift = drift_response.get('is_drift',None)
+
+        if is_drift is not None and is_drift == 1:
+            print("Drift Detected")
+            logger.info("Drift Detected")
 
         result = 'Survived' if prediction == 1 else 'Did not Survive'
 
